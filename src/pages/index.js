a55c767a03002565ecb8ebb5a932ca80906f5c3f
Chris Owen <cowen1992@hotmail.co.uk>
2020-07-29 15:01:25 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -108,13 +108,26 @@ function Home() {
             </p>
             <h2 id="contributing">Creative Commons Open-Source License</h2>
             <p>
-              This work is licensed under a Creative Commons
-              Attribution-ShareAlike 4.0 International License.
+              This work is licensed under a{" "}
+              <a
+                href="https://creativecommons.org/licenses/by-sa/4.0/"
+                target="_blank"
+              >
+                Creative Commons Attribution-ShareAlike 4.0 International
+                License
+              </a>
+              .
             </p>
-            <img
-              src="https://mirrors.creativecommons.org/presskit/buttons/88x31/png/by-sa.png"
-              alt="Creative Commons Open-Source License"
-            />
+            <a
+              href="https://creativecommons.org/licenses/by-sa/4.0/"
+              target="_blank"
+            >
+              <img
+                src="https://mirrors.creativecommons.org/presskit/buttons/88x31/png/by-sa.png"
+                alt="Creative Commons Open-Source License"
+                width="200px"
+              />
+            </a>
           </div>
         </section>
       </main>
