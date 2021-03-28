eb5271df7ee136eb078a5fcaaf4698bc30ef11df
Antigoni Makri <makanti@gmail.com>
2021-03-28 20:56:03 +0100

    remove exercises folder - add solutions in tabs

--- docs/js-core-2/week-1/InClass/Exercises/A-objects-intro/exercise-part-0.js
+++
@@ -1,7 +0,0 @@
-/*
-
-Describe your own laptop as a JavaScript object
-
-Try to think of as many properties as you can!
-
-*/
--- docs/js-core-2/week-1/InClass/Exercises/A-objects-intro/exercise-part-1.js
+++
@@ -1,9 +0,0 @@
-/*
-
-Think of 5 different real world "things" that you 
-can describe with a JavaScript object
-
-Assign each of them to a separate variable
-
-*/
-
